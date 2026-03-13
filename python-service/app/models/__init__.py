9508e43141f53109ed59dea85735b1d7a4961fb0
ereibor <etunere@gmail.com>
2026-03-13 07:59:51 +0100

    clean up code

--- python-service/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.sample_item import SampleItem
-from app.models.briefing import Briefing, BriefingKeyPoint, BriefingMetric, BriefingRisk
-
-__all__ = ["SampleItem", "Briefing", "BriefingKeyPoint", "BriefingRisk", "BriefingMetric"]
